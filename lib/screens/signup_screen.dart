75c8b8c28367f1e832bf575cf03f43a5e2511e0a
Yczar <babalolagbogo@gmail.com>
2023-02-22 15:38:45 +0100

    chore: assignment marked

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -18,59 +18,61 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          title: const Text(
-            "Sign Up",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          "Sign Up",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStingToColor("CB2B93"),
+          hexStingToColor("9546C4"),
+          hexStingToColor("5E61F4")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 10, 0),
+            child: Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Username", Icons.person_outline, false,
+                    _userNameTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter E-mail ID", Icons.person_outline,
+                    false, _emailTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                reusableTextField("Enter Password", Icons.lock_outline, true,
+                    _passwordTextController),
+                const SizedBox(
+                  height: 20,
+                ),
+                signInSignUpButton(context, false, () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: ((context) => const HomeScreen()),
+                    ),
+                  );
+                })
+              ],
+            ),
           ),
         ),
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [
-            hexStingToColor("CB2B93"),
-            hexStingToColor("9546C4"),
-            hexStingToColor("5E61F4")
-          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-          child: SingleChildScrollView(
-            child: Padding(
-                padding: EdgeInsets.fromLTRB(
-                    20, MediaQuery.of(context).size.height * 0.2, 10, 0),
-                child: Column(
-                  children: <Widget>[
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    reusableTextField("Enter Username", Icons.person_outline,
-                        false, _userNameTextController),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    reusableTextField("Enter E-mail ID", Icons.person_outline,
-                        false, _emailTextController),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    reusableTextField("Enter Password", Icons.lock_outline,
-                        true, _passwordTextController),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    signInSignUpButton(context, false, () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: ((context) => const HomeScreen()),
-                        ),
-                      );
-                    })
-                  ],
-                )),
-          ),
-        ));
+      ),
+    );
   }
 }
